3c5f1655d3324ac47dd7e9c45f64274d3ffcd5ec
caualcruz <caualucas34492@gmail.com>
2024-11-19 19:27:54 -0300

    ATIVIDADE EM SALA 5 - CONCLUIDA

--- JAVASCRIPT/index.js
+++ JAVASCRIPT/index.js
@@ -1,8 +1,9 @@
 const divForms = document.getElementById('forms')
+const divTab = document.getElementById('divTab')
 const input = divForms.querySelectorAll('input')
 const btn = divForms.querySelector('button')
 const cor = document.getElementById('cor')
-
+const btnTab = document.getElementById('subTab')
 const body = document.getElementsByTagName('body')
 
 console.log(divForms, input, btn, body)
@@ -41,7 +42,35 @@ function mudarCor() {
         cor.textContent = 'Claro';
     }
 }
-
 cor.addEventListener('click', mudarCor);
 
+btnTab.addEventListener('click', ()=>{
+    const col1 = document.createElement('div')
+    const col2 = document.createElement('div')
+
+    col1.classList.add('coluna');
+    col2.classList.add('coluna');
+    
+    const tab = parseInt(document.getElementById('num3').value);
+    
+    let result = ''
+    let result2 = ''
+    for (let i = 1; i <= 5; i++) {
+        result += `
+        ${tab} x ${i} = ${tab*i} <br>
+        `
+    }
+    for (let i = 6; i <= 10; i++) {
+        result2 += `
+        ${tab} x ${i} = ${tab*i} <br>
+        `
+    }
+
+    col1.innerHTML = result
+    col2.innerHTML = result2
+
+    divTab.innerHTML = ''
+    divTab.appendChild(col1)
+    divTab.appendChild(col2)
+})
 
